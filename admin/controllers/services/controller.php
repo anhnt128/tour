277d1d9dd71d64dad2db0ee5ef0b4ae39edfe950
Tran Van Luan <tran.van.luan@framgia.com>
2014-12-09 00:04:01 +0700

    temp

--- admin/controllers/services/controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if(isset($_GET['act'])){
-		switch($_GET['act']){
-			case "edit" : require("controllers/services/edit_services.php");
-			break;
-		}
-	}
-?>
--- admin/process.php
+++ admin/process.php
@@ -1,18 +1,18 @@
 <?php
-	session_start();
+    session_start();
     require("../libraries/config.php");
-	require("../libraries/database.php");
-	require("libraries/user.php");
-	$user = $_POST['username'];
-	$pass = md5($_POST['password']);
-	$db = new libraries_user;
-	$data = $db->check_login($user,$pass);
-	if($data == FALSE){
-		echo "false";
-	}else{
-		echo "true";
-		$_SESSION['ses_userid'] = $data['user_id'];
-		$_SESSION['ses_user'] = $data['user_name'];
-		$_SESSION['ses_level'] = $data['user_level'];
-	}	
+    require("../libraries/database.php");
+    require("libraries/users.php");
+    $user = $_POST['username'];
+    $pass = md5($_POST['password']);
+    $db = new libraries_users;
+    $data = $db->check_login($user, $pass);
+    if ($data == FALSE) {
+        echo "false";
+    } else {
+        echo "true";
+        $_SESSION['ses_userid'] = $data['user_id'];
+        $_SESSION['ses_user'] = $data['user_name'];
+        $_SESSION['ses_level'] = 1;
+    }
 ?>
